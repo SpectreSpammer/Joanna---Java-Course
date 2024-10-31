b4c8dd58c3e62c7097e3ba694f661026fdb4f96b
Joanna <ningllanos12@gmail.com>
2024-11-01 01:36:44 +0800

    Hello world - Nov. 1 2024

--- src/main/java/day2_java_basic_syntax/MainJavaBasics.java
+++ src/main/java/day2_java_basic_syntax/MainJavaBasics.java
@@ -3,5 +3,6 @@ package day2_java_basic_syntax;
 public class MainJavaBasics {
     public static void main(String[] args) {
         System.out.println(" Hello Joanna!");
+        System.out.println(" Hello world!");
     }
 }
